ec4738137364a1b6cbb9037fa61da9cf7c8f473a
ÍtaloRyan <italoryansouza2019@gmail.com>
2024-06-04 21:28:29 -0300

    Possível solução
    Uma forma de capturar qual o valor dos radios está selecionado

--- asstes/scripts/script.js
+++ asstes/scripts/script.js
@@ -1,3 +1,11 @@
-function escolheu(){
-    window.alert("aaa");
+real1.addEventListener (selecionou(), 'onclick')
+
+function selecionou(){
+    var valor = document.getElementsByName("chor1");
+    for (var i = 0; i < valor.length; i++){
+        if(valor[i].checked){
+            cifra.innerHTML = valor[i].value;
+        }
+    }
 }
+
