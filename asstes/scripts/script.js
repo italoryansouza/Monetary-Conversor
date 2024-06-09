9ea8b8e15b70a9d07030cf3c17b4829f697c2a71
ÍtaloRyan <italoryansouza2019@gmail.com>
2024-06-09 13:11:14 -0300

    Script finalizado

--- asstes/scripts/script.js
+++ asstes/scripts/script.js
@@ -20,97 +20,146 @@ function calcular(){
         }
     }
 
-    number = getElementById('ivalor')
-    numberval = number.value;
+    number = document.getElementById('ivalor')
+    numberval = Number(number.value);
 
     // Cálculos:
 
     //Reais
     if(vde == "R$" && vpara == "real2"){
         resultado.style.display = 'block'
-        resultado.innerHTML = String('aaaaa')
+        var conta = numberval*1
+        resultado.innerHTML = String(`R$${numberval.toFixed(2).replace('.',',')} equivale(m) a <strong>R$</strong>${conta.toFixed(2).replace('.',',')}`)
     }
+
     else if(vde == "R$" && vpara == "euro2"){
         resultado.style.display = 'block'
-        resultado.innerHTML = 
+        var conta = numberval/5.73
+        resultado.innerHTML = String(`R$${numberval.toFixed(2).replace('.',',')} equivale(m) a <strong>€</strong> ${conta.toFixed(2).replace('.',',')}`)
     }
     else if(vde == "R$" && vpara == "libra2"){
-        resultado.innerHTML = 
+        resultado.style.display = 'block'
+        var conta = numberval/6.79
+        resultado.innerHTML = String(`R$${numberval.toFixed(2).replace('.',',')} equivale(m) a <strong>£</strong> ${conta.toFixed(2).replace('.',',')}`)
     }
     else if(vde == "R$" && vpara == "iene2"){
-        resultado.innerHTML = 
+        resultado.style.display = 'block'
+        var conta = numberval/0.034
+        resultado.innerHTML = String(`R$${numberval.toFixed(2).replace('.',',')} equivale(m) a <strong>¥</strong> ${conta.toFixed(2).replace('.',',')}`)
     }
     else if(vde == "R$" && vpara == "dolar2"){
-        resultado.innerHTML = 
+        resultado.style.display = 'block'
+        var conta = numberval/5.35
+        resultado.innerHTML = String(`R$${numberval.toFixed(2).replace('.',',')} equivale(m) a <strong>$</strong> ${conta.toFixed(2).replace('.',',')}`)
     }
+    
 
     //Euro
     if(vde == "€" && vpara == "real2"){
-        resultado.innerHTML = 'Você escolheu Euro no De e Reais no Para'
+        resultado.style.display = 'block'
+        var conta = numberval*5.73
+        resultado.innerHTML = String(`€${numberval.toFixed(2).replace('.',',')} equivale(m) a <strong>R$</strong>${conta.toFixed(2).replace('.',',')}`)
     }
     else if(vde == "€" && vpara == "euro2"){
-        resultado.innerHTML = 'Você escolheu Euro no De e Euro no Para'
+        resultado.style.display = 'block'
+        var conta = numberval*1
+        resultado.innerHTML = String(`€${numberval.toFixed(2).replace('.',',')} equivale(m) a <strong>€</strong>${conta.toFixed(2).replace('.',',')}`)
     }
     else if(vde == "€" && vpara == "libra2"){
-        resultado.innerHTML = 'Você escolheu Euro no De e libra no Para'
+        resultado.style.display = 'block'
+        var conta = numberval/1.17
+        resultado.innerHTML = String(`€${numberval.toFixed(2).replace('.',',')} equivale(m) a <strong>£</strong>${conta.toFixed(2).replace('.',',')}`)
     }
     else if(vde == "€" && vpara == "iene2"){
-        resultado.innerHTML = 'Você escolheu Euro no De e Iene no Para'
+        resultado.style.display = 'block'
+        var conta = numberval/0.0059
+        resultado.innerHTML = String(`€${numberval.toFixed(2).replace('.',',')} equivale(m) a <strong>¥</strong>${conta.toFixed(2).replace('.',',')}`)
     }
     else if(vde == "€" && vpara == "dolar2"){
-        resultado.innerHTML = 'Você escolheu Euro no De e Dolar no Para'
+        resultado.style.display = 'block'
+        var conta = numberval/0.92
+        resultado.innerHTML = String(`€${numberval.toFixed(2).replace('.',',')} equivale(m) a <strong>$</strong>${conta.toFixed(2).replace('.',',')}`)
     }
 
     //Libra
     if(vde == "£" && vpara == "real2"){
-        resultado.innerHTML = 'Você escolheu Libra no De e Reais no Para'
+        resultado.style.display = 'block'
+        var conta = numberval/0.15
+        resultado.innerHTML = String(`£ ${numberval.toFixed(2).replace('.',',')} equivale(m) a <strong>R$</strong>${conta.toFixed(2).replace('.',',')}`)
     }
     else if(vde == "£" && vpara == "euro2"){
-        resultado.innerHTML = 'Você escolheu Libra no De e Euro no Para'
+        resultado.style.display = 'block'
+        var conta = numberval/0.85
+        resultado.innerHTML = String(`£ ${numberval.toFixed(2).replace('.',',')} equivale(m) a <strong>€</strong>${conta.toFixed(2).replace('.',',')}`)
     }
     else if(vde == "£" && vpara == "libra2"){
-        resultado.innerHTML = 'Você escolheu Libra no De e libra no Para'
+        resultado.style.display = 'block'
+        var conta = numberval/1
+        resultado.innerHTML = String(`£ ${numberval.toFixed(2).replace('.',',')} equivale(m) a <strong>£</strong>${conta.toFixed(2).replace('.',',')}`)
     }
     else if(vde == "£" && vpara == "iene2"){
-        resultado.innerHTML = 'Você escolheu Libra no De e Iene no Para'
+        resultado.style.display = 'block'
+        var conta = numberval/0.005
+        resultado.innerHTML = String(`£ ${numberval.toFixed(2).replace('.',',')} equivale(m) a <strong>¥</strong>${conta.toFixed(2).replace('.',',')}`)
     }
     else if(vde == "£" && vpara == "dolar2"){
-        resultado.innerHTML = 'Você escolheu Libra no De e Dolar no Para'
+        resultado.style.display = 'block'
+        var conta = numberval/0.79
+        resultado.innerHTML = String(`£ ${numberval.toFixed(2).replace('.',',')} equivale(m) a <strong>$</strong>${conta.toFixed(2).replace('.',',')}`)
     }
 
     //Iene
     if(vde == "¥" && vpara == "real2"){
-        resultado.innerHTML = 'Você escolheu Iene no De e Reais no Para'
+        resultado.style.display = 'block'
+        var conta = numberval/29.32
+        resultado.innerHTML = String(`¥ ${numberval.toFixed(2).replace('.',',')} equivale(m) a <strong>R$</strong>${conta.toFixed(2).replace('.',',')}`)
     }
     else if(vde == "¥" && vpara == "euro2"){
-        resultado.innerHTML = 'Você escolheu Iene no De e Euro no Para'
+        resultado.style.display = 'block'
+        var conta = numberval/169.09
+        resultado.innerHTML = String(`¥ ${numberval.toFixed(2).replace('.',',')} equivale(m) a <strong>€</strong>${conta.toFixed(2).replace('.',',')}`)
     }
     else if(vde == "¥" && vpara == "libra2"){
-        resultado.innerHTML = 'Você escolheu Iene no De e libra no Para'
+        resultado.style.display = 'block'
+        var conta = numberval/199.05
+        resultado.innerHTML = String(`¥ ${numberval.toFixed(2).replace('.',',')} equivale(m) a <strong>£</strong>${conta.toFixed(2).replace('.',',')}`)
     }
     else if(vde == "¥" && vpara == "iene2"){
-        resultado.innerHTML = 'Você escolheu Iene no De e Iene no Para'
+        resultado.style.display = 'block'
+        var conta = numberval/1
+        resultado.innerHTML = String(`¥ ${numberval.toFixed(2).replace('.',',')} equivale(m) a <strong>¥</strong>${conta.toFixed(2).replace('.',',')}`)
     }
     else if(vde == "¥" && vpara == "dolar2"){
-        resultado.innerHTML = 'Você escolheu Iene no De e Dolar no Para'
+        resultado.style.display = 'block'
+        var conta = numberval/156.73
+        resultado.innerHTML = String(`¥ ${numberval.toFixed(2).replace('.',',')} equivale(m) a <strong>$</strong>${conta.toFixed(2).replace('.',',')}`)
     }
 
     // Dólar
 
     if(vde == "$" && vpara == "real2"){
-        resultado.innerHTML = 'Você escolheu dolar no De e Reais no Para'
+        resultado.style.display = 'block'
+        var conta = numberval/0.19
+        resultado.innerHTML = String(`$ ${numberval.toFixed(2).replace('.',',')} equivale(m) a <strong>R$</strong>${conta.toFixed(2).replace('.',',')}`)
     }
     else if(vde == "$" && vpara == "euro2"){
-        resultado.innerHTML = 'Você escolheu dolar no De e Euro no Para'
+        resultado.style.display = 'block'
+        var conta = numberval/1.08
+        resultado.innerHTML = String(`$ ${numberval.toFixed(2).replace('.',',')} equivale(m) a <strong>€</strong>${conta.toFixed(2).replace('.',',')}`)
     }
     else if(vde == "$" && vpara == "libra2"){
-        resultado.innerHTML = 'Você escolheu dolar no De e libra no Para'
+        resultado.style.display = 'block'
+        var conta = numberval/1.27
+        resultado.innerHTML = String(`$ ${numberval.toFixed(2).replace('.',',')} equivale(m) a <strong>£</strong>${conta.toFixed(2).replace('.',',')}`)
     }
     else if(vde == "$" && vpara == "iene2"){
-        resultado.innerHTML = 'Você escolheu dolar no De e Iene no Para'
+        resultado.style.display = 'block'
+        var conta = numberval/1
+        resultado.innerHTML = String(`$ ${numberval.toFixed(2).replace('.',',')} equivale(m) a <strong>¥</strong>${conta.toFixed(2).replace('.',',')}`)
     }
     else if(vde == "$" && vpara == "dolar2"){
-        resultado.innerHTML = 'Você escolheu dolar no De e Dolar no Para'
+        resultado.style.display = 'block'
+        var conta = numberval/0.0064
+        resultado.innerHTML = String(`$ ${numberval.toFixed(2).replace('.',',')} equivale(m) a <strong>$</strong>${conta.toFixed(2).replace('.',',')}`)
     }
 }
-
